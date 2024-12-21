c6e1a75a771641b49a0f62c20dc06d8c6fa5f1cc
Deimek <333olegovich333@gmail.com>
2024-12-21 19:01:48 +0100

    add_chenges

--- script.js
+++ script.js
@@ -1,19 +1,55 @@
 
-let userNumber = prompt('Ente ryour number 1 - 100, please');
+// let userNumber = prompt('Ente ryour number 1 - 100, please');
+// console.log(`User numaber ${userNumber}`);
+
+// if (userNumber === null) {
+//     alert('ok, bye');
+//     console.log('ok, bye');
+// }
+// else if (!userNumber.trim() || isNaN(userNumber) || !Number.isInteger(+userNumber) || +userNumber <= 1) {
+//     alert('Error: invalid number, try egain...!!!');
+//     console.log('Error: invalid number, try egain...!!!');
+// }
+
+// else {
+//     let isPrime = true;
+//     let number = +userNumber;
+
+//     for (let i = 2; i < number; i++) {
+//         if (number % i === 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+
+//     if (isPrime) {
+//         alert(`Cool: Your number is prime...!!! ${number}`);
+//         console.log(`Cool: Your number is prime...!!! ${number}`);
+//     } else {
+//         alert(`Your number is not prime...!!! ${number}`);
+//         console.log(`Your number is not prime...!!! ${number}`);
+//     }
+// }
+
+
+const userNumber = prompt('Enter your number, please');
 console.log(`User numaber ${userNumber}`);
 
 if (userNumber === null) {
     alert('ok, bye');
     console.log('ok, bye');
 }
-else if (!userNumber.trim() || isNaN(userNumber) || !Number.isInteger(+userNumber) || +userNumber <= 1) {
+else if (!userNumber.trim() || isNaN(userNumber) || !Number.isInteger(+userNumber)) {
     alert('Error: invalid number, try egain...!!!');
     console.log('Error: invalid number, try egain...!!!');
+} else if (+userNumber <= 1) {
+    alert(`Your number is not prime...!!! ${userNumber}`);
+    console.log(`Your number is not prime...!!! ${userNumber}`);
 }
 
 else {
     let isPrime = true;
-    let number = +userNumber;
+    const number = +userNumber;
 
     for (let i = 2; i < number; i++) {
         if (number % i === 0) {
@@ -30,5 +66,3 @@ else {
         console.log(`Your number is not prime...!!! ${number}`);
     }
 }
-
-
